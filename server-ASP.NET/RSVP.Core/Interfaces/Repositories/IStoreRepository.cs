7a6bca9537d59684d8e31e342ed26d52b71b5bfc
handue <124148074+handue@users.noreply.github.com>
2025-06-01 20:21:24 -0700

    feat(server) : GetAllStroesWithHoursAndServicesAsync creation, but not used yet

--- server-ASP.NET/RSVP.Core/Interfaces/Repositories/IStoreRepository.cs
+++ server-ASP.NET/RSVP.Core/Interfaces/Repositories/IStoreRepository.cs
@@ -4,11 +4,13 @@ namespace RSVP.Core.Interfaces.Repositories;
 
 public interface IStoreRepository : IRepository<Store>
 {
+
+    
     Task<Store?> GetByStoreIdAsync(string storeId);
     Task<IEnumerable<Store>> GetStoresByLocationAsync(string location);
     Task<IEnumerable<Store>> GetStoresWithServicesAsync();
     Task<IEnumerable<Store>> GetStoresWithHoursAsync();
     Task<bool> ExistsByStoreIdAsync(string storeId);
 
-    
+    // Task<IEnumerable<Store>> GetAllStoresWithHoursAndServicesAsync();
 } 
